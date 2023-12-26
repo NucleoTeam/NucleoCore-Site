a0455db3e45ced93ee4c7c847dd909429aea9bb0
Nathaniel <nathaniel.davidson@gmail.com>
2023-12-25 20:53:18 -0700

    do not open projects in new tab

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,6 @@ export default function Home() {
           <a
               href="https://nucleodb.com"
               className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-              target={"_blank"}
               rel="noopener noreferrer"
           >
             <h2 className={`mb-3 text-2xl font-semibold`}>
@@ -25,7 +24,6 @@ export default function Home() {
           <a
               href="https://thejointforge.com"
               className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-              target={"_blank"}
               rel="noopener noreferrer"
           >
             <h2 className={`mb-3 text-2xl font-semibold`}>
